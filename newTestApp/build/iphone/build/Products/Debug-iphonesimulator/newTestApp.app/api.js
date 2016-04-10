84f03a560aa063274ae035cf4b3df3765b4b944c
dhunter1991 <hunterdylan882@gmail.com>
2016-04-10 03:48:25 -0400

    commit 2

--- newTestApp/build/iphone/build/Products/Debug-iphonesimulator/newTestApp.app/api.js
+++ newTestApp/build/iphone/build/Products/Debug-iphonesimulator/newTestApp.app/api.js
@@ -8,16 +8,16 @@ var Ui = function(UiData){
 		text: UiData.city,
 		height: 50,
 		width: "100%",
-		top: 0,
+		top: 20,
 		font: {fontFamily: "roboto", fontSize: 30, fontWeight: "bold"},
 		textAlign: "center"
 	});
 	var state = Ti.UI.createLabel({
-		backGroundColor: "#82cbf9",
+		backgroundColor: "#82cbf9",
 		text: UiData.state,
 		height: 50,
 		width: "100%",
-		top: 50,
+		top: 70,
 		font: {fontFamily: "roboto", fontSize: 30, fontWeight: "bold"},
 		textAlign: "center"
 	});
@@ -25,39 +25,39 @@ var Ui = function(UiData){
 		backgroundColor: "#9bd6ee",
 		text: "tempreture " + UiData.temp,
 		height: 100,
-		top: 100,
+		top: 120,
 		width: "50%",
 		left: 0,
 		font: {fontFamily: "roboto", fontSize: 25, fontWeight: "bold"},
 		textAlign: "center"
 	});
 	var feel = Ti.UI.createLabel({
-		backgrounColor: "#115df7",
+		backgrounColor: "#9bd6ee",
 		text: "feels like " + UiData.feelsLike,
 		height: 100,
-		top: 100,
+		top: 120,
 		width: "50%",
 		right: 0,
 		font: {fontFamily: "roboto", fontSize: 25, fontWeight: "bold"},
 		textAlign: "center"
 	});
 	var humidity = Ti.UI.createLabel({
-		backgroundcolor: "#115df7",
+		backgroundcolor: "#9bd6ee",
 		text: "relative humidity " + UiData.humidity,
 		height: 100,
-		top: 250,
+		top: 270,
 		width: "100%",
 		font: {fontFamily: "roboto", fontSize: 20, fontWeight: "bold"},
 		textAlign: "center"
 	});
 	var wind = Ti.UI.createLabel({
 		text: "wind speed " + UiData.wind + " mph",
-		top: 350
+		top: 370
 		
 	});
 	var icon = Ti.UI.createImageView({
 		image: UiData.icon ,
-		top: 400,
+		top: 420,
 	});
 	mainWin.add(wind, humidity, feel, temp, city, state, icon);
 	mainWin.open();
