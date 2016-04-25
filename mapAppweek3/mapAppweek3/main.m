96ea42c3ba88498cbc2173c96b70a7e387d93af5
dhunter1991 <hunterdylan882@gmail.com>
2016-04-25 18:02:49 -0400

    commit 1

---
+++ mapAppweek3/mapAppweek3/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mapAppweek3
+//
+//  Created by Dylan hunter on 4/25/16.
+//  Copyright © 2016 jennifer McCarrick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ week3mapapp/week3mapapp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  week3mapapp
+//
+//  Created by Dylan hunter on 4/25/16.
+//  Copyright © 2016 jennifer McCarrick. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
